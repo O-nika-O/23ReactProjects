e066bd830aad7c65741a5b2760ec52c4e335ddc0
nika <cv3910@innovaccion.mx>
2021-09-22 19:27:09 -0500

    end of second project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    //in case of being false
+    setLoading(true);
+    //so we can adress any kind of network error
+    try {
+      const response = await fetch(url);
+      //Now we have an array of JSON objcts
+      const tours = await response.json();
+      //we switch, we dont see the loading screen anymore
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if(tours.length === 0){
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={fetchTours}>
+          refresh
+        </button>
+      </div>
+    </main>
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
