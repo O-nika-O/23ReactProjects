ca99dd4231b60e5b04761ddf3cca22ada1ef5389
nika <cv3910@innovaccion.mx>
2021-10-01 03:43:49 -0500

    15th project almost done

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lore m liber
+        liber metus et just e iac odio just elit. Lore Lorem Ips.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
